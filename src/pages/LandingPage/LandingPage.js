7833e0951f1e31c829ccfbce9fc7161b2a6388b6
PranayB003 <pranayborgohain@gmail.com>
2022-06-30 15:54:27 +0530

    feat: completed landing page gradient part

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,13 +1,36 @@
+import { Stack, Typography } from "@mui/material";
 import React from "react";
 import styles from "./LandingPage.module.css";
 
 const LandingPage = () => {
     return (
-        <div style={{ display: "flex" }}>
+        <div className={styles["container-div"]}>
             <div
                 className={`${styles["landing-gradient-bg"]} ${styles["gradient-div"]}`}
-            ></div>
-            <div className={`${styles["login-div"]}`}>Hemllo</div>
+                styles={{
+                    display: "flex",
+                    alignItems: "center",
+                    border: "3px solid red",
+                }}
+            >
+                <Stack>
+                    <Typography textAlign="center" variant="h1" color="#FFFFFF">
+                        <b>BEST</b>
+                    </Typography>
+                    <Typography textAlign="center" variant="h4" color="#FFFFFF">
+                        <b>of</b>
+                    </Typography>
+                    <Typography variant="h1" textAlign="center" color="#FFFFFF">
+                        <b>
+                            <i>Both</i>
+                        </b>
+                    </Typography>
+                    <Typography variant="h1" color="#FFFFFF" textAlign="center">
+                        <b>Worlds</b>
+                    </Typography>
+                </Stack>
+            </div>
+            <div className={`${styles["login-div"]}`}></div>
         </div>
     );
 };
