869f8660961d514d53e07a70ec22a9d94f61f75a
PranayB003 <pranayborgohain@gmail.com>
2022-07-15 09:21:54 +0530

    chatRoom reducer function first draft

--- src/hooks/useChatRoom.js
+++ src/hooks/useChatRoom.js
@@ -7,36 +7,79 @@ import messageConverter from "../utils/messageConverter";
 const globalChatRef = collection(db, "globalChat").withConverter(
     messageConverter
 );
-
-const userPhoneNumber = auth.currentUser.phoneNumber;
-const phoneNumber = `${userPhoneNumber.slice(0, -2)}XX`;
-// const userDisplayName = auth.currentUser.displayName;
-
 const initialState = {
-    sideBar: {
-        category: "global",
-        title: "Global Server",
-        selected: "chatroom",
+    server: {
+        id: "global",
+        name: "Global Server",
     },
-    mainContent: {
-        title: "Global Chat",
-        chatCollectionRef: globalChatRef,
-        senderID: phoneNumber,
+    channel: {
+        id: "chatroom",
+        name: "Global Chat",
+    },
+    chat: {
+        collectionRef: globalChatRef,
+        senderID: `${auth.currentUser.phoneNumber.slice(0, -2)}XX`,
     },
 };
 
-const chatReducer = (state, action) => {
-    return;
+const chatReducer = (oldState, action) => {
+    const displayName = auth.currentUser.displayName;
+    const phoneNumber = `${auth.currentUser.phoneNumber.slice(0, -2)}XX`;
+
+    if (action.type === "server") {
+        const server = action.payload.server;
+        if (server.id === oldState.server.id) {
+            return oldState;
+        } else if (server.id === "global") {
+            return {
+                ...initialState,
+                chat: {
+                    collectionRef: globalChatRef,
+                    senderID: phoneNumber,
+                },
+            };
+        } else {
+            return {
+                server,
+                channel: {
+                    id: undefined,
+                    name: "",
+                },
+                chat: {
+                    collectionRef: undefined,
+                    senderID: undefined,
+                },
+            };
+        }
+    } else if (action.type === "channel") {
+        const channel = action.payload.channel;
+        if (channel.id === oldState.channel.id) {
+            return oldState;
+        } else {
+            return {
+                server: oldState.server,
+                channel,
+                chat: {
+                    collectionRef: action.payload.chat.collectionRef,
+                    senderID:
+                        oldState.server.id === "global"
+                            ? phoneNumber
+                            : displayName,
+                },
+            };
+        }
+    } else {
+        console.log("Invalid chatRoom action: " + action.type);
+        return oldState;
+    }
 };
 
 const ChatRoomContext = React.createContext(initialState);
 export const ChatContextProvider = (props) => {
-    const [chatState, dispatch] = useReducer(chatReducer, initialState);
+    const [state, dispatch] = useReducer(chatReducer, initialState);
 
     return (
-        <ChatRoomContext.Provider
-            value={{ state: chatState, dispatch: dispatch }}
-        >
+        <ChatRoomContext.Provider value={{ state, dispatch }}>
             {props.children}
         </ChatRoomContext.Provider>
     );
