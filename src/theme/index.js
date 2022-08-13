f4ad6e83a2ef20684feb9f4771ed0a473cef8539
Pranay Borgohain <66739544+PranayB003@users.noreply.github.com>
2022-08-13 19:40:24 +0530

    Remove dead code

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,11 +1,9 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
 import palette from "./palette";
-// import typography from "./typography";
 
 let theme = createTheme({
     palette,
-    // typography,
     shape: { borderRadius: 10, borderColor: "#FFFFFF" },
 });
 
